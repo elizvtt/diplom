baa91cef859588e4c69d1197066fb37ccc96a6c8
elizvtt <likzaa332@gmail.com>
2026-05-16 00:17:48 +0300

    /// secrets

--- app/Services/GeminiService.php
+++ app/Services/GeminiService.php
@@ -64,19 +64,3 @@ Requirements:
 }
 
 // Gemini 2.5 Pro
-
-// curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent" \
-//   -H 'Content-Type: application/json' \
-//   -H 'X-goog-api-key: AIzaSyDqyqJP03XHjyBycgAK4LE0QyJ2yXRoz4o' \
-//   -X POST \
-//   -d '{
-//     "contents": [
-//       {
-//         "parts": [
-//           {
-//             "text": "Explain how AI works in a few words"
-//           }
-//         ]
-//       }
-//     ]
-//   }'
